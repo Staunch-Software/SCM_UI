6448820e71f20b52fd741391d42247c37be55ed9
Staunch-Software <Software@staunchtec.com>
2025-11-13 18:21:09 +0530

    API changes

--- src/components/InventoryHub.jsx
+++ src/components/InventoryHub.jsx
@@ -16,7 +16,7 @@ const InventoryHub = () => {
     const fetchHubData = async () => {
       try {
         setLoading(true);
-        const response = await fetch("http://127.0.0.1:8000/api/inventory-hub");
+        const response = await fetch("https://odooerp.staunchtec.com/api/inventory-hub");
         if (!response.ok) {
           throw new Error(`API Error: ${response.statusText}`);
         }
